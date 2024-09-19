1d87b204e71cf11999de29ccb6db76ec87c67173
Stanislao95 <166648506+Stanislao95@users.noreply.github.com>
2024-09-19 21:41:11 +0200

    esercizi dom

---
+++ manipolazione dom/index.js
@@ -0,0 +1,17 @@
+const button= document.createElement("button");
+button.innerText="Aggiungi";
+const div= document.querySelector("div");
+div.appendChild(button);
+const addProduct=(()=>{
+  const input= ducument.querySelector("input");
+  const ul= document.querySelector("li");
+  const checkbox= document.querySelector("checkbox");
+  checkbox.setAttribute("input" , "checkbox");
+  const li= createElement("li");
+  li.innerText = inner.value;
+  li.appendChild("checkbox");
+  li.appendChild("li");
+  input.value= ("");
+});
+
+button.addEventListener("click", addProduct)
---
+++ manipolazione dom/navigazionedom.js/dom2.js
@@ -0,0 +1,6 @@
+const paragrafo= document.querySelectorAll(".paragrafo")
+console.log(document.querySelectorAll(".paragrafo"))
+const li= document.querySelector("#elemento-3")
+console.log(ocument.querySelector("#elemento-3"))
+const h2= document.querySelectorAll(".h2")
+
